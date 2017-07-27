0ff3bed51b1ea37370a10e33905ccd8045195056
Niklaas Baudet von Gersdorff <stdin@niklaas.eu>
2017-07-27 21:55:16 +0200

    Adapts variables to blink's convention

--- colors/blink-default.js
+++ colors/blink-default.js
@@ -1,4 +1,4 @@
-term_.prefs_.set('background-color', "DEFAULT");
-term_.prefs_.set('foreground-color', "DEFAULT");
-term_.prefs_.set('cursor-color', "DEFAULT");
-term_.prefs_.set('color-palette-overrides', []);
+t.prefs_.set('background-color', "DEFAULT");
+t.prefs_.set('foreground-color', "DEFAULT");
+t.prefs_.set('cursor-color', "DEFAULT");
+t.prefs_.set('color-palette-overrides', []);
