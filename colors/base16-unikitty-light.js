35b2c71cbd884b5799d002d537172803b05e2879
Rufo Sanchez <rufo@rufosanchez.com>
2018-03-24 12:27:24 -0400

    Fix color reference

--- colors/base16-unikitty-light.js
+++ colors/base16-unikitty-light.js
@@ -1,7 +1,7 @@
 // Base16 Unikitty Light
 // Scheme: Josh W Lewis (@joshwlewis)
 
-var color_scheme = {
+var = {
         'base00': '#ffffff',
         'base01': '#e1e1e2',
         'base02': '#c4c3c5',
@@ -20,30 +20,30 @@ var color_scheme = {
         'base0F': '#e013d0',
 };
 
-term_.prefs_.set('background-color', color_scheme.base00);
-term_.prefs_.set('foreground-color', color_scheme.base05);
+term_.prefs_.set('background-color', base00);
+term_.prefs_.set('foreground-color', base05);
 term_.prefs_.set('cursor-color', "rgba(108, 105, 110, 0.5)");
 
 term_.prefs_.set('color-palette-overrides', 
-                        [color_scheme.base00,
-                        color_scheme.base08,
-                        color_scheme.base0B,
-                        color_scheme.base0A,
-                        color_scheme.base0D,
-                        color_scheme.base0E,
-                        color_scheme.base0C,
-                        color_scheme.base05,
-                        color_scheme.base03,
-                        color_scheme.base08,
-                        color_scheme.base0B,
-                        color_scheme.base0A,
-                        color_scheme.base0D,
-                        color_scheme.base0E,
-                        color_scheme.base0C,
-                        color_scheme.base07,
-                        color_scheme.base09,
-                        color_scheme.base0F,
-                        color_scheme.base01,
-                        color_scheme.base02,
-                        color_scheme.base04,
-                        color_scheme.base06]);
+                        [base00,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base05,
+                        base03,
+                        base08,
+                        base0B,
+                        base0A,
+                        base0D,
+                        base0E,
+                        base0C,
+                        base07,
+                        base09,
+                        base0F,
+                        base01,
+                        base02,
+                        base04,
+                        base06]);
